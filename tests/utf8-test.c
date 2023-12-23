f4053c148c5627242cd862e8ae074915544f41b6
Loris Kriyonas <loris.kriyonas@gmail.com>
2023-12-23 15:14:16 +0100

    Testing the utf-8 api

--- tests/utf8-test.c
+++ tests/utf8-test.c
@@ -1,45 +1,79 @@
 #include <locale.h>
-#include <stdio.h>
 
+#include "clib/asserts.h"
 #include "datatypes/bytes.h"
 #include "datatypes/utf8.h"
 
-int main(void) {
+#include <string.h>
+
+void test_decode(void) {
+  Bytes bytes = BYTES(        //
+      0xF0, 0x9F, 0x8E, 0x89, // 🎉
+      0xE2, 0x9C, 0x85,       // ✅
+      0xF0, 0x9F, 0x98, 0x81, // 😁
+      0x43,                   // C
+      0x41                    // A
+  );
+  Utf8 s3 = utf8_decode(bytes);
+  clib_assert(memcmp(s3.data, "🎉✅😁CA", s3.size) == 0,
+              "Was not decoded correctly");
+
+  Bytes garbage = BYTES(0xF0, 0x01);
+  Utf8 res = {0};
+  bool ret = utf8_try_decode(garbage, &res);
+  clib_assert(ret == false, "This should not be a valid utf-8 string");
+}
+
+void test_creation(void) {
+  Utf8 s = UTF8("🎉✅😁 Hellö  Wörld 💩");
+  clib_assert(memcmp(s.data, "🎉✅😁 Hellö  Wörld 💩", s.size) == 0,
+              "Was not created correctly");
+}
+
+void test_copy(void) {
   Arena arena = {0};
+  Utf8 s = UTF8("🎉✅😁 Hellö  Wörld 💩");
+  Utf8 s2 = utf8_copy(s, &arena);
+  clib_assert(utf8_eq(s, s2) == true, "Utf8 strings not equal");
+  clib_assert(memcmp(s2.data, "🎉✅😁 Hellö  Wörld 💩", s.size) == 0,
+              "Was not created correctly");
+  arena_free(&arena);
+}
+
+void test_cmp(void) {
+  Utf8 s1 = UTF8("🎉✅😁CA");
+  Utf8 s2 = UTF8("🎉✅😁CA");
+  Utf8 s3 = UTF8("🎉✅✅CA");
+  clib_assert(utf8_eq(s1, s2) == true, "Strings should be equal");
+  clib_assert(utf8_eq(s1, s3) == false, "Strings should not be equal");
+}
+
+void test_next(void) {
+  Utf8 test = UTF8("🎉✅😁CA");
+  Utf8 one = utf8_next(&test);
+  clib_assert(utf8_eq(one, UTF8("🎉")), "Utf-8 was not correctly choped!");
+  one = utf8_next(&test);
+  clib_assert(utf8_eq(one, UTF8("✅")), "Utf-8 was not correctly choped!");
+  one = utf8_next(&test);
+  clib_assert(utf8_eq(one, UTF8("😁")), "Utf-8 was not correctly choped!");
+
+  bool ret = utf8_try_next(&test, &one);
+  clib_assert(utf8_eq(one, UTF8("C")), "Utf-8 was not correctly choped!");
+  clib_assert(ret, "Utf-8 was not correctly choped!");
+  ret = utf8_try_next(&test, &one);
+  clib_assert(utf8_eq(one, UTF8("A")), "Utf-8 was not correctly choped!");
+  clib_assert(ret, "Utf-8 was not correctly choped!");
+
+  ret = utf8_try_next(&test, &one);
+  clib_assert(ret == false, "Utf-8 was not correctly choped!");
+}
+
+int main(void) {
   setlocale(LC_ALL, "");
 
-  Utf8 s = UTF8("Hellö Wörld 💩");
-  printf(UTF8_FMT "\n", UTF8_ARG(s));
-
-  Utf8 s2 = utf8_copy(s, &arena);
-  printf(UTF8_FMT "\n", UTF8_ARG(s2));
-
-  Bytes bytes = BYTES(0xF0, 0x9F, 0x8E, 0x89, 0xE2, 0x9C, 0x85, 0xF0, 0x9F,
-                      0x98, 0x81, 0x43, 0x41);
-  Utf8 s3 = utf8_decode(bytes);
-  printf(UTF8_FMT "\n", UTF8_ARG(s3));
-
-  Utf8 test = utf8_copy(s3, &arena);
-
-  Utf8 one = utf8_next(&test);
-  printf("Rest: " UTF8_FMT "\n", UTF8_ARG(test));
-  printf("One: " UTF8_FMT "\n", UTF8_ARG(one));
-
-  one = utf8_next(&test);
-  printf("Rest: " UTF8_FMT "\n", UTF8_ARG(test));
-  printf("One: " UTF8_FMT "\n", UTF8_ARG(one));
-
-  one = utf8_next(&test);
-  printf("Rest: " UTF8_FMT "\n", UTF8_ARG(test));
-  printf("One: " UTF8_FMT "\n", UTF8_ARG(one));
-
-  one = utf8_next(&test);
-  printf("Rest: " UTF8_FMT "\n", UTF8_ARG(test));
-  printf("One: " UTF8_FMT "\n", UTF8_ARG(one));
-
-  one = utf8_next(&test);
-  printf("Rest: " UTF8_FMT "\n", UTF8_ARG(test));
-  printf("One: " UTF8_FMT "\n", UTF8_ARG(one));
-
-  arena_free(&arena);
+  test_decode();
+  test_creation();
+  test_copy();
+  test_cmp();
+  test_next();
 }
