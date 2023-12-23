8d4c40a2601c8fb05247134633b74fa787e93f96
Loris Kriyonas <loris.kriyonas@gmail.com>
2023-12-23 15:13:24 +0100

    Made the utf-8 api better

--- src/datatypes/utf8.c
+++ src/datatypes/utf8.c
@@ -3,42 +3,102 @@
 #include <string.h>
 
 #include "clib/arena.h"
+#include "clib/asserts.h"
 #include "datatypes/bytes.h"
 
-#define LEADING_TWO_BITS ((uint8_t)0xC0)
-#define LEADING_FIRST_BIT ((uint8_t)0x80)
+#define BITS 8
+#define FIRST_BIT ((uint8_t)0x80)
 
-Utf8 utf8_decode(Bytes bytes) {
-  size_t len = 0;
+static size_t count_left_bits(uint8_t byte) {
+  size_t count = 0;
+  for (size_t i = 0; i < BITS; i++) {
+    if (!(byte & (FIRST_BIT >> i))) {
+      break;
+    }
+    count++;
+  }
+  return count;
+}
+
+bool utf8_validate_bytes(Bytes bytes) {
   for (size_t i = 0; i < bytes.size; i++) {
-    if ((bytes.data[i] & LEADING_TWO_BITS) != LEADING_FIRST_BIT) {
-      len++;
+    size_t bit_count = count_left_bits(bytes.data[i]);
+    clib_assert_return(bit_count <= 4, false);
+    size_t idx = i;
+    for (size_t j = 1; j < bit_count; j++) {
+      clib_assert_return(count_left_bits(bytes.data[idx + j]) == 1, false);
+      i++;
     }
   }
-  return (Utf8){.len = len, .size = bytes.size, .data = (char *)bytes.data};
+  return true;
+}
+
+bool utf8_validate(Utf8 s) {
+  return utf8_validate_bytes(bytes_from_parts(s.size, (uint8_t *)s.data));
+}
+
+bool utf8_try_decode(Bytes bytes, Utf8 *out) {
+  size_t len = 0;
+  for (size_t i = 0; i < bytes.size; i++) {
+    size_t bit_count = count_left_bits(bytes.data[i]);
+    clib_assert_return(bit_count <= 4, false);
+    size_t idx = i;
+    for (size_t j = 1; j < bit_count; j++) {
+      clib_assert_return(count_left_bits(bytes.data[idx + j]) == 1, false);
+      i++;
+    }
+    len++;
+  }
+  out->len = len;
+  out->size = bytes.size;
+  out->data = (char *)bytes.data;
+  return true;
+}
+
+Utf8 utf8_decode(Bytes bytes) {
+  Utf8 str = {0};
+  bool ret = utf8_try_decode(bytes, &str);
+  clib_assert(ret, "Decoding Utf-8 failed");
+  return str;
+}
+
+bool utf8_try_encode(Utf8 s, Bytes *out) {
+  bool ret = utf8_validate(s);
+  clib_assert_return(ret, false);
+  *out = bytes_from_parts(s.size, (uint8_t *)s.data);
+  return true;
 }
 
 Bytes utf8_encode(Utf8 s) {
+  bool ret = utf8_validate(s);
+  clib_assert(ret, "Encoding Utf8 failed");
   return bytes_from_parts(s.size, (uint8_t *)s.data);
 }
 
-Utf8 utf8_next(Utf8 *str) {
-  if (str->len < 2) {
-    Utf8 res = *str;
-    *str = (Utf8){0};
-    return res;
+bool utf8_eq(Utf8 s1, Utf8 s2) {
+  if (s1.len != s2.len || s1.size != s2.size) {
+    return false;
   }
-  size_t i = 0;
-  size_t len = 0;
-  for (; i < str->size && len < 2; i++) {
-    if ((str->data[i] & LEADING_TWO_BITS) != LEADING_FIRST_BIT) {
-      len++;
-    }
-  }
-  Utf8 res = (Utf8){.len = 1, .size = i - 1, .data = str->data};
-  str->size -= i - 1;
+  return memcmp(s1.data, s2.data, s1.size) == 0;
+}
+
+bool utf8_try_next(Utf8 *str, Utf8 *out) {
+  size_t bit_count = count_left_bits(str->data[0]);
+  clib_assert_return(bit_count <= 4, false);
+  clib_assert_return(bit_count != 1, false);
+  size_t bytes = bit_count == 0 ? 1 : bit_count;
+  clib_assert_return(bytes <= str->size, false);
+  *out = (Utf8){.len = 1, .size = bytes, .data = str->data};
+  str->size -= bytes;
   str->len--;
-  str->data += i - 1;
+  str->data += bytes;
+  return true;
+}
+
+Utf8 utf8_next(Utf8 *str) {
+  Utf8 res = {0};
+  bool ret = utf8_try_next(str, &res);
+  clib_assert(ret, "Getting next character failed");
   return res;
 }
 
--- src/datatypes/utf8.h
+++ src/datatypes/utf8.h
@@ -4,17 +4,24 @@
 #include "clib/arena.h"
 #include "datatypes.h" // IWYU pragma: private: include "utf8.h"
 
-// TODO utf8_validate()
-
-#define UTF8(s) utf8_decode(BYTES_STR(s));
+#define UTF8(s) utf8_decode(BYTES_STR(s))
 #define UTF8_FMT "%.*s"
 #define UTF8_ARG(s) (int)(s).size, (s).data
 
-size_t utf8_cstrlen(const char *cstr);
+bool utf8_try_decode(Bytes bytes, Utf8 *out);
 Utf8 utf8_decode(Bytes bytes);
 
+bool utf8_try_encode(Utf8 s, Bytes *out);
+Bytes utf8_encode(Utf8 s);
+
+bool utf8_eq(Utf8 s1, Utf8 s2);
+
+bool utf8_try_next(Utf8 *str, Utf8 *out);
 Utf8 utf8_next(Utf8 *str);
 
 Utf8 utf8_copy(Utf8 str, Arena *arena);
+
+bool utf8_validate(Utf8 s);
+bool utf8_validate_bytes(Bytes s);
 
 #endif /* ifndef __CLIB_UTF8_H__ */
