781d55616cb2d8f893d00cecf57551dc70f8c294
Loris Kriyonas <loris.kriyonas@gmail.com>
2023-12-24 09:29:07 +0100

    added tests for endianess

--- tests/int-test.c
+++ tests/int-test.c
@@ -9,14 +9,27 @@ void test_u8_leading_bits(void) {
   clib_assert(u8_leading_zeros(b2) == 4, "Did not count correctly");
 }
 
-void test_u8__swaping_bits(void) {
+void test_u8_swaping_bits(void) {
   const u8 b = 0x80;
   clib_assert(u8_reverse_bits(b) == 0x01, "Did not reverse correctly");
   const u8 b2 = 0x01;
   clib_assert(u8_reverse_bits(b2) == 0x80, "Did not reverse correctly");
 }
 
+void test_u8_endian(void) {
+#if CLIB_BYTE_ORDER == ENDIAN_BIG
+  printf("BIG\n");
+  clib_assert(u8_to_le(0x80) == 0x01, "Bits were not swapped");
+  clib_assert(u8_to_be(0x80) == 0x80, "Bits are somehow different")
+#else
+  printf("BIG\n");
+  clib_assert(u8_to_le(0x80) == 0x80, "Bits are somehow different");
+  clib_assert(u8_to_be(0x80) == 0x01, "Bits are somehow different");
+#endif
+}
+
 int main(void) {
   test_u8_leading_bits();
-  test_u8__swaping_bits();
+  test_u8_swaping_bits();
+  test_u8_endian();
 }
