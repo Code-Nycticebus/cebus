4eeb287336fa9fe5b1aed98c845df8bd7c087499
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-01-01 17:02:08 +0100

    Added io api

---
+++ src/clib/io.c
@@ -0,0 +1,67 @@
+#include "io.h"
+#include "clib/asserts.h"
+#include "datatypes/bytes.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+bool file_try_open(const char *filename, const char *mode, File *file) {
+  file->handle = fopen(filename, mode);
+  clib_assert_return(file->handle, false);
+  return true;
+}
+
+File file_open(const char *filename, const char *mode) {
+  File file = {0};
+  clib_assert(file_try_open(filename, mode, &file),
+              "Could not open file: %s: %s", filename, strerror(errno));
+  return file;
+}
+
+void file_close(File *file) {
+  fclose(file->handle);
+  file->handle = NULL;
+}
+
+void file_rewind(File *file) {
+  clib_assert_debug(file->handle, "Invalid file handle!");
+  rewind(file->handle);
+}
+
+usize file_size(File *file) {
+  const usize current = ftell(file->handle);
+  fseek(file->handle, 0, SEEK_END);
+  const usize size = ftell(file->handle);
+  fseek(file->handle, current, SEEK_SET);
+  return size;
+}
+
+bool file_try_read_bytes(File *file, Arena *arena, Bytes *bytes) {
+  clib_assert_debug(file->handle, "Invalid file handle!");
+  const usize size = file_size(file);
+  u8 *buffer = arena_alloc(arena, size);
+  const usize bytes_read = fread(buffer, sizeof(u8), size, file->handle);
+  *bytes = bytes_from_parts(size, buffer);
+  if (bytes_read < size) {
+    return false;
+  }
+  return true;
+}
+bool file_stream_bytes(File *file, usize chunk_size,
+                       void (*stream)(Bytes bytes)) {
+  clib_assert_debug(file->handle, "Invalid file handle!");
+  Arena arena = {0};
+
+  u8 *buffer = arena_alloc(&arena, chunk_size);
+  while (!feof(file->handle)) {
+    const usize size = fread(buffer, sizeof(u8), chunk_size, file->handle);
+    if (ferror(file->handle)) {
+      return false;
+    }
+    stream(bytes_from_parts(size, buffer));
+  }
+
+  arena_free(&arena);
+  return true;
+}
---
+++ src/clib/io.h
@@ -0,0 +1,33 @@
+#ifndef __CLIB_IO_H__
+#define __CLIB_IO_H__
+
+#include "clib/arena.h"
+#include "clib/platform.h"
+#include "datatypes/datatypes.h"
+
+#if defined(LINUX)
+#include <bits/types/FILE.h>
+#else
+#error "Not on windows right now"
+#endif
+
+typedef enum {
+  FILE_OK,
+  FILE_OPEN,
+} FileError;
+
+typedef struct {
+  FILE *handle;
+} File;
+
+bool file_try_open(const char *filename, const char *mode, File *file);
+File file_open(const char *filename, const char *mode);
+void file_close(File *file);
+
+void file_rewind(File *file);
+
+bool file_try_read_bytes(File *file, Arena *arena, Bytes *bytes);
+bool file_stream_bytes(File *file, usize chunk_size,
+                       void (*stream)(Bytes bytes));
+
+#endif // !__CLIB_IO_H__
