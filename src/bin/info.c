85729801ba02144ac7f74bc654e727af506c0b17
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-01-12 01:38:15 +0100

    Added info binary

---
+++ src/bin/info.c
@@ -0,0 +1,15 @@
+
+#include "clib/logging.h"
+#include "core/platform.h"
+
+int main(void) {
+  clib_log_info("%s-%s", CLIB_SYSTEM, CLIB_ARCHITECTURE);
+  clib_log_info("%s", CLIB_COMPILER);
+#if defined(CLIB_64BIT)
+  clib_log_info("64-bit");
+#else
+  clib_log_info("Bit:\t32-bit");
+#endif
+  clib_log_info("%s endian",
+                CLIB_BYTE_ORDER == ENDIAN_LITTLE ? "little" : "big");
+}
