20b4e4525f85f9fe804010d1b6fa09e5976e28c2
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-01-12 23:57:41 +0100

    Formatting

--- src/core/sorting.h
+++ src/core/sorting.h
@@ -5,7 +5,6 @@
 
 void quicksort(void *src, void *dest, usize size, usize nmem,
                CompareFn compare);
-
 void quicksort_ctx(void *src, void *dest, usize size, usize nmem,
                    CompareCtxFn compare, const void *ctx);
 
