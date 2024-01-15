b3b8a3868255a90749b69f0fdc708fd2c3131944
Loris Kriyonas <loris.kriyonas@gmail.com>
2024-01-15 12:51:42 +0100

    Removed warnings in qs-test.c

--- tests/qs-test.c
+++ tests/qs-test.c
@@ -7,10 +7,15 @@ static CmpOrdering cmp(const void *a, const void *b) {
 
 // Example usage
 int main(void) {
-  i32 array[] = {10, 9, 8, 7, 6, 4, 3, 5, 1};
+  const i32 array[] = {10, 9, 8, 7, 6, 4, 3, 5, 1};
   usize n = sizeof(array) / sizeof(array[0]);
-  quicksort(array, array, sizeof(i32), n, cmp);
-  clib_assert(array[0] == 1, "Array was not sorted correctly");
+
+  i32 sorted[n];
+  quicksort(array, sorted, sizeof(i32), n, cmp);
+  clib_assert(sorted[0] == 1, "Array was not sorted correctly");
+  clib_assert(sorted[1] == 3, "Array was not sorted correctly");
+  clib_assert(sorted[2] == 4, "Array was not sorted correctly");
+  clib_assert(sorted[3] == 5, "Array was not sorted correctly");
 
   return 0;
 }
