e4078fe5b82ff8be14efb649f93343554ef0a4ad
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-01-17 13:18:24 +0100

    implemented windows os impl

--- src/os/windows/os.c
+++ src/os/windows/os.c
@@ -1,5 +1,26 @@
 #include "../os.h"
 
 #if defined(WINDOWS)
-#error "Not implemented"
+
+#include "types/str.h"
+#include "types/integers.h"
+#include <string.h>
+#include <windows.h>
+#include <direct.h>
+
+void os_chdir(Str path) {
+  char buffer[MAX_PATH] = {0};
+  memcpy(buffer, path.data, usize_min(path.len, MAX_PATH));
+  _chdir(buffer);
+}
+
+Str os_getcwd(Arena *arena) {
+  usize size = GetCurrentDirectory(0, NULL);
+  char *buf = arena_alloc(arena, size);
+  GetCurrentDirectory((DWORD)size, buf);
+  // TODO error handling
+  return str_from_parts(size, buf);
+}
+
+
 #endif
