7f8f33de7e1ef3874c63ed28c21603e527e6c999
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-01-20 13:47:18 +0100

    Restructured integer operations

--- src/types/integers.c
+++ src/types/integers.c
@@ -5,6 +5,7 @@
 #include "types/bytes.h"
 
 #define INTEGER_IMPL(T, BITS)                                                  \
+  /* BIT OPERATIONS */                                                         \
   T T##_reverse_bits(T value) {                                                \
     T reversed = 0;                                                            \
     for (usize i = 0; i < BITS; i++) {                                         \
@@ -15,18 +16,6 @@
       value = value >> 1;                                                      \
     }                                                                          \
     return reversed;                                                           \
-  }                                                                            \
-                                                                               \
-  T T##_swap_bytes(T value) {                                                  \
-    if (1 < sizeof(T)) {                                                       \
-      u8 *bytes = (u8 *)&value;                                                \
-      for (usize i = 0; i < (sizeof(T) + 1) / 2; i++) {                        \
-        u8 temp = bytes[i];                                                    \
-        bytes[i] = bytes[sizeof(T) - i - 1];                                   \
-        bytes[sizeof(T) - i - 1] = temp;                                       \
-      }                                                                        \
-    }                                                                          \
-    return value;                                                              \
   }                                                                            \
                                                                                \
   usize T##_leading_ones(T value) {                                            \
@@ -92,7 +81,22 @@
     }                                                                          \
     return count;                                                              \
   }                                                                            \
+  /* BIT OPERATIONS END */                                                     \
                                                                                \
+  /* BYTE ORDER OPERATIONS */                                                  \
+  T T##_swap_bytes(T value) {                                                  \
+    if (1 < sizeof(T)) {                                                       \
+      u8 *bytes = (u8 *)&value;                                                \
+      for (usize i = 0; i < (sizeof(T) + 1) / 2; i++) {                        \
+        u8 temp = bytes[i];                                                    \
+        bytes[i] = bytes[sizeof(T) - i - 1];                                   \
+        bytes[sizeof(T) - i - 1] = temp;                                       \
+      }                                                                        \
+    }                                                                          \
+    return value;                                                              \
+  }                                                                            \
+                                                                               \
+  /*  BIG ENDIAN OPERATIONS */                                                 \
   T T##_to_be(T value) {                                                       \
     if (CLIB_BYTE_ORDER == ENDIAN_LITTLE) {                                    \
       return T##_swap_bytes(value);                                            \
@@ -130,6 +134,7 @@
     return bytes_from_parts(sizeof(value), buffer);                            \
   }                                                                            \
                                                                                \
+  /*  LITTLE ENDIAN OPERATIONS */                                              \
   T T##_to_le(T value) {                                                       \
     if (CLIB_BYTE_ORDER == ENDIAN_BIG) {                                       \
       return T##_swap_bytes(value);                                            \
@@ -167,6 +172,7 @@
     return bytes_from_parts(sizeof(value), buffer);                            \
   }                                                                            \
                                                                                \
+  /*  NATIVE ENDIAN OPERATIONS */                                              \
   T T##_from_ne_bytes(Bytes bytes) {                                           \
     clib_assert(sizeof(T) == bytes.size,                                       \
                 "expected %" USIZE_FMT " bytes but got %" USIZE_FMT,           \
@@ -180,7 +186,17 @@
     }                                                                          \
     return T##_to_le_bytes(value, arena);                                      \
   }                                                                            \
+  /* BYTE ORDER OPERATIONS END */                                              \
                                                                                \
+  /* MATH OPERATIONS */                                                        \
+  T T##_max(T a, T b) { return a < b ? b : a; }                                \
+  T T##_min(T a, T b) { return a > b ? b : a; }                                \
+  T T##_clamp(T min, T max, T value) {                                         \
+    return value < min ? min : max < value ? max : value;                      \
+  }                                                                            \
+  /* MATH OPERATIONS END */                                                    \
+                                                                               \
+  /* UTILS */                                                                  \
   u64 T##_hash(T value) {                                                      \
     u64 hash = ((u64)value) + 1;                                               \
     hash = (((hash >> 16) ^ hash) % 0x3AA387A8B1) * 0x45d9f3b;                 \
@@ -193,12 +209,6 @@
     T temp = *v1;                                                              \
     *v1 = *v2;                                                                 \
     *v2 = temp;                                                                \
-  }                                                                            \
-                                                                               \
-  T T##_max(T a, T b) { return a < b ? b : a; }                                \
-  T T##_min(T a, T b) { return a > b ? b : a; }                                \
-  T T##_clamp(T min, T max, T value) {                                         \
-    return value < min ? min : max < value ? max : value;                      \
   }                                                                            \
                                                                                \
   CmpOrdering T##_compare_lt(T a, T b) {                                       \
@@ -219,7 +229,7 @@
                                                                                \
   CompareFn T##_compare_qsort(CmpOrdering ordering) {                          \
     return ordering == CMP_LESS ? _##T##_cmp_lt : _##T##_cmp_gt;               \
-  }
+  } /* UTILS END */
 
 INTEGER_IMPL(u8, U8_BITS)
 INTEGER_IMPL(i8, I8_BITS)
--- src/types/integers.h
+++ src/types/integers.h
@@ -5,135 +5,46 @@
 #include "core/defines.h"
 
 #define INTEGER_DECL(T)                                                        \
-  /*                                                                           \
-   * Returns a new integer with its bits reversed.                             \
-   */                                                                          \
+  /* BIT OPERATIONS */                                                         \
   T T##_reverse_bits(T value);                                                 \
-                                                                               \
-  /*                                                                           \
-   * Returns a new integer with its bytes reversed.                            \
-   */                                                                          \
-  T T##_swap_bytes(T value);                                                   \
-                                                                               \
-  /*                                                                           \
-   * Returns the count of leading ones.                                        \
-   */                                                                          \
   usize T##_leading_ones(T value);                                             \
-                                                                               \
-  /*                                                                           \
-   * Returns the count of trailing ones.                                       \
-   */                                                                          \
   usize T##_trailing_ones(T value);                                            \
-                                                                               \
-  /*                                                                           \
-   * Returns the count of leading zeros.                                       \
-   */                                                                          \
   usize T##_leading_zeros(T value);                                            \
-                                                                               \
-  /*                                                                           \
-   * Returns the count of trailing zeros.                                      \
-   */                                                                          \
   usize T##_trailing_zeros(T value);                                           \
-                                                                               \
-  /*                                                                           \
-   *  Counts bits that are set to one.                                         \
-   */                                                                          \
   usize T##_count_zeros(T value);                                              \
-                                                                               \
-  /*                                                                           \
-   *  Counts bits that are set to one.                                         \
-   */                                                                          \
   usize T##_count_ones(T value);                                               \
+  /* BIT OPERATIONS END */                                                     \
                                                                                \
-  /*                                                                           \
-   * Returns new integer in big-endian byte order from native byte order.      \
-   */                                                                          \
+  /* BYTE ORDER OPERATIONS */                                                  \
+  T T##_swap_bytes(T value);                                                   \
+  /*  BIG ENDIAN OPERATIONS */                                                 \
   T T##_to_be(T value);                                                        \
-                                                                               \
-  /*                                                                           \
-   * Returns new integer in native byte order from big-endian byte order.      \
-   */                                                                          \
   T T##_from_be(T value);                                                      \
-                                                                               \
-  /*                                                                           \
-   * Returns new integer in native byte order from big-endian byte order.      \
-   */                                                                          \
   T T##_from_be_bytes(Bytes bytes);                                            \
-                                                                               \
-  /*                                                                           \
-   * Returns Bytes in big-endian byte order from integer.                      \
-   */                                                                          \
   Bytes T##_to_be_bytes(T value, Arena *arena);                                \
-                                                                               \
-  /*                                                                           \
-   * Returns new integer in little-endian byte order from native byte order.   \
-   */                                                                          \
+  /*  LITTLE ENDIAN OPERATIONS */                                              \
   T T##_to_le(T value);                                                        \
-                                                                               \
-  /*                                                                           \
-   * Returns new integer in native byte order from little-endian byte order.   \
-   */                                                                          \
   T T##_from_le(T value);                                                      \
-                                                                               \
-  /*                                                                           \
-   * Returns new integer in native byte order from little-endian byte order.   \
-   */                                                                          \
   T T##_from_le_bytes(Bytes bytes);                                            \
-                                                                               \
-  /*                                                                           \
-   * Returns Bytes in little-endian byte order from integer.                   \
-   */                                                                          \
   Bytes T##_to_le_bytes(T value, Arena *arena);                                \
-                                                                               \
-  /*                                                                           \
-   * Returns new integer in native byte order from native byte order.          \
-   */                                                                          \
+  /*  NATIVE ENDIAN OPERATIONS */                                              \
   T T##_from_ne_bytes(Bytes bytes);                                            \
-                                                                               \
-  /*                                                                           \
-   * Returns Bytes in native byte order from integer.                          \
-   */                                                                          \
   Bytes T##_to_ne_bytes(T value, Arena *arena);                                \
+  /* BYTE ORDER OPERATIONS END */                                              \
                                                                                \
-  /*                                                                           \
-   *  Returns the bigger out of 'a' and 'b'.                                   \
-   */                                                                          \
+  /* MATH OPERATIONS */                                                        \
   T T##_max(T a, T b);                                                         \
-                                                                               \
-  /*                                                                           \
-   *  Returns the smaller out of 'a' and 'b'.                                  \
-   */                                                                          \
   T T##_min(T a, T b);                                                         \
-                                                                               \
-  /*                                                                           \
-   * Returns 'value' clamped to 'min' and 'max'.                               \
-   */                                                                          \
   T T##_clamp(T min, T max, T value);                                          \
+  /* MATH OPERATIONS END */                                                    \
                                                                                \
-  /*                                                                           \
-   * Basic hash.                                                               \
-   */                                                                          \
+  /* UTILS */                                                                  \
   u64 T##_hash(T value);                                                       \
-                                                                               \
-  /*                                                                           \
-   * Swaps two integer variables.                                              \
-   */                                                                          \
   void T##_swap(T *v1, T *v2);                                                 \
-                                                                               \
-  /*                                                                           \
-   * Compares less than.                                                       \
-   */                                                                          \
   CmpOrdering T##_compare_lt(T a, T b);                                        \
-                                                                               \
-  /*                                                                           \
-   * Compares greater than.                                                    \
-   */                                                                          \
   CmpOrdering T##_compare_gt(T a, T b);                                        \
-                                                                               \
-  /*                                                                           \
-   * Returns a function that you can pass to qsort.                            \
-   */                                                                          \
-  CompareFn T##_compare_qsort(CmpOrdering ordering);
+  CompareFn T##_compare_qsort(CmpOrdering ordering);                           \
+  /* UTILS END */
 
 INTEGER_DECL(u8)
 INTEGER_DECL(i8)
