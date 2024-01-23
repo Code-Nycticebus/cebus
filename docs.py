e4d246c6e5224f4a74d9a27015492bb2773ba3fc
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-01-23 22:13:38 +0100

    Overengineering docs.py

--- docs.py
+++ docs.py
@@ -17,6 +17,7 @@ README = DOC / "README.md"
 def write_src_to_doc(doc: TextIO, src: Path):
     first = True
     with open(src, "r") as d:
+        # This is kind of a state machine
         write_mode: bool = False
         for line in d.readlines():
             if "*/" in line:
@@ -33,35 +34,36 @@ def write_src_to_doc(doc: TextIO, src: Path):
 
 
 def main() -> None:
-    docs_dict: dict[Path, list[Path]] = defaultdict(list)
-
     # Collects the files
+    docs_dict: dict[Path, list[Path]] = defaultdict(list)
     for file in SRC.rglob("*.h"):
         doc = DOC / file.relative_to(SRC).parent
         docs_dict[doc].append(file)
 
+    # Removes any doc.py created documentation
     with open(README, "r") as f:
-        # Removes any previous concatinated documentation
         lines_to_keep = []
         for line in f.readlines():
             lines_to_keep.append(line)
             if "<!-- DOCUMENTATION -->" in line:
                 break
 
-    sorted_entries = tuple(
-        (docs, tuple(sorted(files))) for docs, files in sorted(docs_dict.items())
-    )
-
     # Creates table of content
     with open(README, "w") as f:
         for line in lines_to_keep:
             f.write(line)
+
+        sorted_entries = tuple(
+            (docs, tuple(sorted(files))) for docs, files in sorted(docs_dict.items())
+        )
+
+        # Creates table of content
         for docs, files in sorted_entries:
             f.write(f"- [{docs.name.title()}](#{docs.name.title()})\n")
             for file in files:
                 f.write(f"   - [{file.name}](#{file.name.replace('.', '')})\n")
 
-        # Iterates over all files and writes their content to readme
+        # Writes documentation content
         for docs, files in sorted_entries:
             f.write(f"## {docs.name.title()}\n")
 
