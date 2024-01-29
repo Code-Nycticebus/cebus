9fd25275c77dbff1f91818ab25a2e4a8e5fed728
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-01-29 09:32:29 +0100

    Reworked my ll draft

---
+++ tests/collections/ll-test.c
@@ -0,0 +1,61 @@
+#include "core/arena.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct LLNode {
+  struct LLNode *next, *prev;
+} LLNode;
+
+typedef struct {
+  LLNode *begin, *end;
+} LinkedList;
+
+void ll_append(LinkedList *ll, LLNode *node);
+
+/////////////////////////////////////////////////////////////
+
+void ll_append(LinkedList *ll, LLNode *node) {
+  if (ll->begin == NULL) {
+    ll->begin = node;
+  }
+  if (ll->end != NULL) {
+    node->prev = ll->end;
+    ll->end->next = node;
+  }
+  ll->end = node;
+}
+
+/////////////////////////////////////////////////////////////
+
+typedef struct {
+  LLNode head;
+  int value;
+} IntNode;
+
+static LLNode *int_node_init(int value, Arena *arena) {
+  IntNode *node = arena_calloc(arena, sizeof(IntNode));
+  node->value = value;
+  return (LLNode *)node;
+}
+
+int main(void) {
+  Arena arena = {0};
+
+  LinkedList ll = {0};
+
+  for (usize i = 0; i < 10; ++i) {
+    ll_append(&ll, int_node_init(rand() % 255, &arena));
+  }
+
+  printf("{");
+  for (LLNode *next = ll.begin; next; next = next->next) {
+    IntNode *node = (IntNode *)next;
+    if (next != ll.begin) {
+      printf(", ");
+    }
+    printf("%d", node->value);
+  }
+  printf("}\n");
+
+  arena_free(&arena);
+}
