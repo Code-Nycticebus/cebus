be653426df5e2d7841ee84a9cf4f12627b399c29
Loris Kriyonas <loris.kriyonas@gmail.com>
2024-02-22 19:44:46 +0100

    Made the IWYU pragmas better

--- src/clib/clib.h
+++ src/clib/clib.h
@@ -1,24 +1,26 @@
 #ifndef __CLIB_H__
 #define __CLIB_H__
 
-#include "clib/collection/da.h"      // IWYU pragma: export
-#include "clib/collection/hashmap.h" // IWYU pragma: export
-#include "clib/collection/set.h"     // IWYU pragma: export
-#include "clib/core/arena.h"         // IWYU pragma: export
-#include "clib/core/assert.h"        // IWYU pragma: export
-#include "clib/core/error.h"         // IWYU pragma: export
-#include "clib/core/logging.h"       // IWYU pragma: export
-#include "clib/core/platform.h"      // IWYU pragma: export
-#include "clib/os/cmd.h"             // IWYU pragma: export
-#include "clib/os/dll.h"             // IWYU pragma: export
-#include "clib/os/fs.h"              // IWYU pragma: export
-#include "clib/os/io.h"              // IWYU pragma: export
-#include "clib/os/os.h"              // IWYU pragma: export
-#include "clib/type/byte.h"          // IWYU pragma: export
-#include "clib/type/char.h"          // IWYU pragma: export
-#include "clib/type/float.h"         // IWYU pragma: export
-#include "clib/type/integer.h"       // IWYU pragma: export
-#include "clib/type/string.h"        // IWYU pragma: export
-#include "clib/type/utf8.h"          // IWYU pragma: export
+// IWYU pragma: begin_exports
+
+#include "clib/collection/da.h"
+#include "clib/collection/hashmap.h"
+#include "clib/collection/set.h"
+#include "clib/core/arena.h"
+#include "clib/core/assert.h"
+#include "clib/core/error.h"
+#include "clib/core/logging.h"
+#include "clib/core/platform.h"
+#include "clib/os/cmd.h"
+#include "clib/os/dll.h"
+#include "clib/os/fs.h"
+#include "clib/os/io.h"
+#include "clib/os/os.h"
+#include "clib/type/byte.h"
+#include "clib/type/char.h"
+#include "clib/type/float.h"
+#include "clib/type/integer.h"
+#include "clib/type/string.h"
+#include "clib/type/utf8.h"
 
 #endif /* !__CLIB_H__ */
