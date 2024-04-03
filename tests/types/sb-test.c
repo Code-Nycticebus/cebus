240b056ba31726c647536ccef1f6ff603d699f19
Loris Kriyonas <loris.kriyonas@gmail.com>
2024-04-03 22:17:42 +0200

    Added string builder as a test

---
+++ tests/types/sb-test.c
@@ -0,0 +1,45 @@
+#include "clib/clib.h"
+#include "clib/collection/da.h"
+
+#include <string.h>
+
+typedef struct {
+  DA(char) buffer;
+} StringBuilder;
+
+static StringBuilder sb_init(Arena *arena) {
+  StringBuilder sb = {0};
+  da_init(&sb.buffer, arena);
+  da_push(&sb.buffer, 0);
+  return sb;
+}
+
+static Str sb_to_str(StringBuilder *sb) {
+  return str_from_parts(sb->buffer.len - 1, sb->buffer.items);
+}
+
+static void sb_append_cstr(StringBuilder *sb, const char *cstr) {
+  da_pop(&sb->buffer);
+  da_extend(&sb->buffer, strlen(cstr), cstr);
+  da_push(&sb->buffer, 0);
+}
+
+static void sb_append_str(StringBuilder *sb, Str str) {
+  da_pop(&sb->buffer);
+  da_extend(&sb->buffer, str.len, str.data);
+  da_push(&sb->buffer, 0);
+}
+
+int main(void) {
+  Arena arena = {0};
+  StringBuilder sb = sb_init(&arena);
+
+  sb_append_cstr(&sb, "Hello");
+  sb_append_str(&sb, STR(", "));
+  sb_append_cstr(&sb, "World");
+  Str s = sb_to_str(&sb);
+
+  clib_log_info(STR_FMT, STR_ARG(s));
+
+  arena_free(&arena);
+}
