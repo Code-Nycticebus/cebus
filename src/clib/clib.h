ca004499901472923032b6fd7cc8d72fdce54c96
Loris Kriyonas <loris.kriyonas@gmail.com>
2024-04-07 11:31:39 +0200

    Formatting

--- src/clib/clib.h
+++ src/clib/clib.h
@@ -14,16 +14,19 @@ Include this file to include all the header files that are listed below.
 #include "clib/collection/hashmap.h"
 #include "clib/collection/set.h"
 #include "clib/collection/string_builder.h"
+
 #include "clib/core/arena.h"
 #include "clib/core/assert.h"
 #include "clib/core/error.h"
 #include "clib/core/logging.h"
 #include "clib/core/platform.h"
+
 #include "clib/os/cmd.h"
 #include "clib/os/dll.h"
 #include "clib/os/fs.h"
 #include "clib/os/io.h"
 #include "clib/os/os.h"
+
 #include "clib/type/byte.h"
 #include "clib/type/char.h"
 #include "clib/type/float.h"
