6d0a805b5de1f478a220c9ed66cf5b900cc25f2e
Loris Kriyonas <loris.kriyonas@gmail.com>
2024-05-03 12:51:56 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -1,4 +1,23 @@
-#/bin/bash
-gcc -I./src -c ./src/**/*.c 
-ar rcs libclib.a *.o 
+#!/bin/sh
+
+INSTALL_DIRECTORY="$(pwd)/lib"
+
+mkdir -p "$INSTALL_DIRECTORY"
+mkdir -p "$INSTALL_DIRECTORY"/include
+
+set -e
+
+for f in src/clib/**/*.c
+do 
+  gcc -I./src -c "$f" 
+done
+
+ar rcs lib/libclib.a *.o 
 rm *.o
+
+cp src/clib.h lib/include
+cp -r src/clib/ lib/include 
+for f in lib/include/clib/**/*.c
+do 
+  rm $f
+done
